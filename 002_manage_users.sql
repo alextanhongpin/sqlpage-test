afb5845e2a03c754f9d6fe36c962ba996c0a7e15
alextanhongpin <alex.tan@bandlab.com>
2025-03-15 16:10:07 +0800

    chore: update to use table component

--- 002_manage_users.sql
+++ 002_manage_users.sql
@@ -1,3 +1,8 @@
+CREATE TABLE IF NOT EXISTS users (
+    id int generated always as identity primary key,
+    name text not null
+);
+
 SELECT 
     'form'            AS component,
     '002_manage_users__create_user.sql' AS action;
@@ -5,14 +10,17 @@ SELECT
     'username' AS name;
 
 
-SELECT 'list' AS component,
+SELECT 'table' AS component,
     'Users' AS title,
     'List of users' AS description,
-    false AS compact;
+    'No users found' AS empty_description,
+    true AS sort,
+    true AS search,
+    'Actions' AS markdown; -- Set to true to enable markdown in the column.
 
 SELECT
     id AS id,
     name AS description,
-    '002_manage_users__delete_user.sql?delete=' || id AS delete_link
+    '[Delete](002_manage_users__delete_user.sql?delete=' || id || ')' AS Actions
 FROM users
 LIMIT 10;
